442c2ff2f6abc6de635c9fe08bb87d707d07f406
Vic <vontiverosewhs@gmail.com>
2023-05-09 10:20:39 +0300

    Added slash command

--- server.ts
+++ server.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+import { Client, GatewayIntentBits } from 'discord.js';
+import { TOKEN } from './src/config';
+import { getCommands } from './src/commands';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user!.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
+getCommands();
---
+++ src/commands.ts
@@ -0,0 +1,23 @@
+import { REST, Routes } from 'discord.js';
+import { CLIENT_ID, TOKEN } from './config';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+export const getCommands = async() => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+      
+        await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+      
+        console.log('Successfully reloaded application (/) commands.');
+      } catch (error) {
+        console.error(error);
+      }
+}
