38f02032ae54cd938aa73c237b609ae787af8c30
Vic <vontiverosewhs@gmail.com>
2023-05-10 10:44:39 +0300

    Update dev branch

--- server.ts
+++ server.ts
@@ -7,13 +7,29 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user!.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
+    console.log('Bot has replied with pong!')
   }
 });
+
+client.on("message", async message => {
+    if (message.author.bot) return;
+
+  // Ignore messages not sent in a text channel
+    if (message.channel.type !== 'text') return;
+
+  // Log the message content and author to the console
+    console.log(`[${message.guild.name}][${message.channel.name}] ${message.author.tag}: ${message.content}`);
+})
+
+/*client.on('threadCreate', async () => {
+    console.log('Thread has been created')
+})*/
+
 
 client.login(TOKEN);
 getCommands();
