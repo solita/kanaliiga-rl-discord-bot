3bb5784edd08e6cb01a269090f1c6b5416cd6b4c
Max Caroe <max.caroe@solita.fi>
2023-05-11 09:21:14 +0300

    few test added for ContentController

--- tests/ContentController.test.ts
+++ tests/ContentController.test.ts
@@ -4,14 +4,56 @@ const controller = new ContentController()
 
 describe("Content controller", () => {
 
-    // beforeEach(()=>{
-        
+    const threadId_Test = "Test_ThreadID"
+    const groupId_Test = "Test_GroupID"
+    const groupName_Test = "Test_GroupName"
+
+    beforeEach(()=>{
+        controller.clearTasks()
+    })
+
+    it("New instance of Controller and documentProcessor is defined", ()=>{
+        expect(controller).toBeDefined()
+        expect(controller.processor).toBeDefined()
+    })
+
+
+    it("New ContentQueues are created and added to controllers queue", async ()=>{
+
+        const TASK_COUNT = 4
+
+        for (let i = 0; i < TASK_COUNT ; i++){
+            await controller.createNewTask(threadId_Test+i, groupName_Test+i)
+        }
+
+        expect(controller.tasks.length).toBe(TASK_COUNT)
+        expect(controller.tasks[0].threadId).toBe(threadId_Test+"0")
+        expect(controller.tasks[3].threadId).toBe(threadId_Test+"3")
+        expect(controller.tasks[0].queue.length).toBe(0)
+        expect(controller.tasks[3].queue.length).toBe(0)
+
+    })
+
+    // it("Removes a ContentQueue with a specific thread id from the tasks list", async ()=>{
+    //     await controller.createNewTask(threadId_Test, groupName_Test)
+    //     expect(controller.tasks.length).toBe(1)
+
+
+    //     controller.removeTask(threadId_Test)
+    //     expect(controller.tasks.length).toBe(1)
+
+
     // })
 
+    // TODO: Test for adding new items to a specific ContentQueues queue (URL'S)
+    
+    // TODO: Test for adding new items to a ContentQueue 
+    // that does not exists (expect a new ContentQueue to be created on the fly)
+
+    // TODO: Test for processing all the ContentQueues
+
+    // TODO: Test for removing a ContentQueue from tasks list if its empty and old enough
 
 
-    it("New instance of Controller is created", ()=>{
-        expect(controller).toBeDefined()
-    })
 })
 
