fa4b4577cb112a056e9231ad5e37fad7cee58bc1
Vic <vontiverosewhs@gmail.com>
2023-05-15 14:17:20 +0300

    Work on upload function

--- src/DocumentProcessor.ts
+++ src/DocumentProcessor.ts
@@ -1,6 +1,7 @@
 import { BALL_CHASING_API_KEY } from "./config";
 import log from "./log"
 import https from 'https'
+import FormData from 'form-data';
 //import fs from 'fs'
 //import path from 'path'
 
@@ -11,26 +12,28 @@ export class DocumentProcessor {
         this.filePath = './temp'
     }
 
-    async upload(replay: Buffer): Promise<any> {
+    async upload(replay: Buffer): Promise<Response> {
         log.info(`Uploading....`)
         const url = 'https://ballchasing.com/api/v2/upload?visibility=private';
         const form = new FormData();
-        form.append('data', new Blob([replay]), 'something.replay',)
-        try {
-            const res = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Authorization': BALL_CHASING_API_KEY,
-                    'Content-Type':' multipart/form-data; boundary=---BOUNDARY'
-                },
-                body: form
-            })
-
-            const data = await res.json()
-            return data
-        } catch (err) {
-            console.log(err)
-        }
+        form.append('file', replay)
+        //console.log(form)
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': BALL_CHASING_API_KEY,
+                        'Content-Type': 'multipart/form-data; boundary='+form.getBoundary()
+                    },
+                    //body: form
+                })
+                resolve(res)
+            } catch (err) {
+                console.error(err)
+                reject(err)
+            }
+        })
     }
 
     async download(url: string): Promise<any> {
--- tests/DocumentProcessor.test.ts
+++ tests/DocumentProcessor.test.ts
@@ -1,6 +1,4 @@
 import { DocumentProcessor } from "../src/DocumentProcessor";
-import fs from 'fs'
-import log from "../src/log";
 
 describe("Document processor", () => {
     const url = 'https://media.discordapp.net/attachments/1106549031691362404/1106549107209801728/Screenshot_2023-03-15_at_8.32.30.png'
@@ -13,22 +11,22 @@ describe("Document processor", () => {
 
     it("Downloads a file into a buffer", async () => {
         const buffer = await processor.download(url)
-        console.log(buffer)
         expect(buffer).toBeDefined();
     })
 
     it("Upload a replay file to Ballchasing", async() => {
-        const data = fs.readFile('/src/Small_Giants_vs_Elisa_Fiffers_10052023_round_1.replay', async (err, buffer) => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            const data = await processor.upload(buffer)
-            return data
-        })
-        
+        const testBuffer = Buffer.alloc(5)
+        const res = await processor.upload(testBuffer);
+        const data = await res.json()
+        expect(res).toBeDefined();
         expect(data).toBeDefined();
+
+        if (res.status === 500 || 400) {
+            console.log(data)
+            expect(data).toHaveProperty('error')
+        } else {
+            expect(data).toHaveProperty('id')
+            expect(data).toHaveProperty('location')
+        }
     })
 })
-
-//TODO MERGE FROM MAIN!!
