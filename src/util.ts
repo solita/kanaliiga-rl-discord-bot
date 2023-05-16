ae7487e51905e6d9e1054de66301dbb9d30c0179
Vic <vontiverosewhs@gmail.com>
2023-05-16 15:10:47 +0300

    getDivisionName function implemented

--- src/util.ts
+++ src/util.ts
@@ -22,3 +22,9 @@ export const allAttahcmentsAreCorrectType =
         return true
     }
 
+export const getDivisionName = (postTitle: string) => {
+    const splitString = postTitle.split(', ')
+    //splits into array to get division name, for example: [ 'Solita Ninja vs Solita Herkku', 'Challengers', '1.5.2023' ]
+    const postTitleDivisionName = splitString[1]
+    return postTitleDivisionName
+}    
--- util/fileNameParser.ts
+++
@@ -1,6 +0,0 @@
-export const getDivisionName = (postTitle: string) => {
-    const splitString = postTitle.split(', ')
-    //splits into array to get division name, for example: [ 'Solita Ninja vs Solita Herkku', 'Challengers', '1.5.2023' ]
-    const postTitleDivisionName = splitString[1]
-    return postTitleDivisionName
-}
