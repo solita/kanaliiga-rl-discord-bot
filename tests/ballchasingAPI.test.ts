77c3f2e2cbf5e4b030870eee005c2cbbe80ceb1d
Max Caroe <max.caroe@solita.fi>
2023-05-17 12:16:19 +0300

    test for parsing fetch groups results added

--- tests/ballchasingAPI.test.ts
+++ tests/ballchasingAPI.test.ts
@@ -1,11 +1,15 @@
-import { fetchGroups } from "../src/ballchasingAPI";
+import { fetchGroups, searchGroupId } from "../src/ballchasingAPI";
 import { mockResponse } from "./testHelpers";
 
 
 
 describe("Ballchasing Api", () => {
 
-    
+    /*
+    These tests depend on mockResponse from testHelpers.ts
+
+    Please see the mockresponse for more details on existing mocked groups
+    */
 
     it("Fetches the groups from ballchasing API", async () => {
 
@@ -21,7 +25,24 @@ describe("Ballchasing Api", () => {
 
     })
 
-    // TODO: more tests
+
+    it("Parses the group fetch response into: [possibleMatch, [other, results]]", async()=>{
+
+        global.fetch = jest.fn().mockImplementationOnce(() => {
+            return Promise.resolve(mockResponse)
+        })
+
+
+        const response = await fetchGroups()
+
+        const [matchId, allResults] = searchGroupId('Challengers',response)
+
+        expect(matchId).toBe('12345Test')
+        expect(allResults[0]).toBe('Challengers')
+        expect(allResults[1]).toBe('league2')
+
+
+    })
 
 
 })
