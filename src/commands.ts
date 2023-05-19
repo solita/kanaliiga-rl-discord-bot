c0b9497b84ca6fc9072fe3084eba34eaa932ae6b
Max Caroe <max.caroe@solita.fi>
2023-05-19 09:10:19 +0300

    prettier indent tab setting update

--- src/commands.ts
+++ src/commands.ts
@@ -5,58 +5,63 @@ import { pingBCApi } from './ballchasingAPI';
 import { EmbedBuilder } from 'discord.js';
 
 const commands = [
-  {
-    name: 'health',
-    description: 'Replies with bot health and status'
-  }
+    {
+        name: 'health',
+        description: 'Replies with bot health and status'
+    }
 ];
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 export const getCommands = async () => {
-  try {
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        await rest.put(Routes.applicationCommands(CLIENT_ID), {
+            body: commands
+        });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
+export const botHealth = async (
+    controller: ContentController,
+    client: Client
+): Promise<EmbedBuilder> => {
+    const story: string[] = [];
+    const ballchasinStatus = await pingBCApi();
+    const isBotHealthy = ballchasinStatus.status === 200;
 
-export const botHealth = async (controller: ContentController, client: Client): Promise<EmbedBuilder> => {
+    story.push(`Bot is ${isBotHealthy ? 'healthy 🫶' : 'not healthy 😮‍💨'} `);
+    story.push(`Uptime ${(client.uptime / 1000 / 86400).toFixed(2)} days`);
 
-  const story:string[] = []
-  const ballchasinStatus = await pingBCApi()
-  const isBotHealthy = ballchasinStatus.status === 200
+    if (!isBotHealthy) {
+        story.push(
+            `Connection to ballchasing seems to have a problem: \n${ballchasinStatus.url} - ${ballchasinStatus.status} - ${ballchasinStatus.statusText}`
+        );
+    } else {
+        story.push(
+            `Connection to Ballchasin.com is OK! - ${ballchasinStatus.status}`
+        );
+    }
 
-  story.push(`Bot is ${isBotHealthy ? 'healthy 🫶' : 'not healthy 😮‍💨'} `)
-  story.push(`Uptime ${((client.uptime / 1000)/86400).toFixed(2)} days`)
+    story.push(`Bot has ${controller.tasks.length} posts stored in memory`);
 
-  if (!isBotHealthy){
-    story.push(`Connection to ballchasing seems to have a problem: \n${ballchasinStatus.url} - ${ballchasinStatus.status} - ${ballchasinStatus.statusText}`)
-  }
-  else{
-    story.push(`Connection to Ballchasin.com is OK! - ${ballchasinStatus.status}`)
-  }
+    controller.tasks.forEach((task) => {
+        story.push(
+            ` Post id ${task.thread.id} with ${task.queue.length} messages in queue`
+        );
+        task.queue.forEach((message) => {
+            message.attachments.forEach((r) => {
+                story.push(`   Attachment ${r.name}`);
+            });
+        });
+    });
 
-  story.push(`Bot has ${controller.tasks.length} posts stored in memory`)
-  
-  controller.tasks.forEach(task => {
-    story.push(` Post id ${task.thread.id} with ${task.queue.length} messages in queue`)
-    task.queue.forEach(message => {
-      message.attachments.forEach(r => {
-        story.push(`   Attachment ${r.name}`)
-      })
-    })
-  })
+    const embedContainer = new EmbedBuilder()
+        .setColor(isBotHealthy ? '#53b33b' : '#d12020')
+        .setTitle(`Kanaliiga RL Discord Bot ${APPLICATION_VERSION}`)
+        .setURL('https://github.com/solita/kanaliiga-rl-discord-bot')
+        .setDescription(story.join('\n'));
 
-  const embedContainer = new EmbedBuilder()
-  .setColor(isBotHealthy ? '#53b33b' : '#d12020')
-  .setTitle(`Kanaliiga RL Discord Bot ${APPLICATION_VERSION}`)
-  .setURL('https://github.com/solita/kanaliiga-rl-discord-bot')
-  .setDescription(story.join('\n'))
-
-  return embedContainer
-}
-
-
-
+    return embedContainer;
+};
