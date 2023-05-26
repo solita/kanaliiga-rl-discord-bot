bd283e84847a8ce8606d37152b9493e3914c24d9
Max Caroe <max.caroe@solita.fi>
2023-05-26 10:03:28 +0300

    switch added

--- server.ts
+++ server.ts
@@ -1,11 +1,10 @@
 import { Client, GatewayIntentBits, Events } from 'discord.js';
-import { ADMIN_ROLE, bcParentGroup, TOKEN } from './src/config';
+import { ADMIN_ROLE, TOKEN } from './src/config';
 import { getCommands } from './src/commands/commands';
 import { ContentController } from './src/ContentController';
-import { fetchGroups, reportBcApiConnection } from './src/ballchasingAPI';
+import { reportBcApiConnection } from './src/ballchasingAPI';
 import { hasRole } from './src/util';
-import log from './src/log';
-import { botHealth, divisionHelp } from './src/commands/interactions/util';
+import { botHealth, divisionHelp } from './src/commands/interactions/embeds';
 import { handleParentSetCommand } from './src/commands/interactions/rl_setparent';
 import { handleCheckCommand } from './src/commands/interactions/rl_check';
 
@@ -27,43 +26,53 @@ client.on(Events.ClientReady, async () => {
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'rl_health') {
-        const infoEmbed = await botHealth(controller, client);
-        await interaction.reply({ embeds: [infoEmbed] });
-    } else if (interaction.commandName === 'rl_divisionhelp') {
-        const divisionHelpEmbed = await divisionHelp();
-        await interaction.reply({ embeds: [divisionHelpEmbed] });
-    } else if (interaction.commandName === 'rl_setparent') {
-        const guild = client.guilds.cache.get(interaction.guild.id);
-        guild.members.fetch(interaction.user.id).then(async (member) => {
-            if (hasRole(member.roles.cache, ADMIN_ROLE)) {
-                handleParentSetCommand(interaction);
-            } else {
-                interaction.reply(
-                    `Only admins (${ADMIN_ROLE}) can update this.`
-                );
-            }
-        });
-    } else if (interaction.commandName === 'rl_check') {
-        const guild = client.guilds.cache.get(interaction.guild.id);
-        const isRoleEnough = await guild.members
-            .fetch(interaction.user.id)
-            .then((member) => hasRole(member.roles.cache, ADMIN_ROLE));
+    switch (interaction.commandName){
 
-        if (!isRoleEnough) {
-            interaction.reply(`Only admins (${ADMIN_ROLE}) can run checks.`);
-            return;
+        case 'rl_health':
+            const infoEmbed = await botHealth(controller, client);
+            await interaction.reply({ embeds: [infoEmbed] });
+            break
+
+        case 'rl_divisionhelp':
+            const divisionHelpEmbed = await divisionHelp();
+            await interaction.reply({ embeds: [divisionHelpEmbed] });   
+            break
+
+        case 'rl_setparent':{
+            const guild = client.guilds.cache.get(interaction.guild.id);
+            guild.members.fetch(interaction.user.id).then(async (member) => {
+                if (hasRole(member.roles.cache, ADMIN_ROLE)) {
+                    handleParentSetCommand(interaction);
+                } else {
+                    interaction.reply(
+                        `Only admins (${ADMIN_ROLE}) can update this.`
+                    )}
+            });
+            break
         }
-        interaction.reply('On it!');
+        
+        case 'rl_check':{
+            const guild = client.guilds.cache.get(interaction.guild.id);
+            const isRoleEnough = await guild.members
+                .fetch(interaction.user.id)
+                .then((member) => hasRole(member.roles.cache, ADMIN_ROLE));
 
-        //From ChannelManager, fetch all channels
-        const channels = client.channels.cache;
+            if (!isRoleEnough) {
+                interaction.reply(`Only admins (${ADMIN_ROLE}) can run checks.`);
+                return;
+            }
+            interaction.reply('On it!');
 
-        const promises = await handleCheckCommand(channels, controller);
+            //From ChannelManager, fetch all channels
+            const channels = client.channels.cache;
 
-        Promise.all(promises).then(() => {
-            controller.processQueue();
-        });
+            const jobAddTasks = await handleCheckCommand(channels, controller);
+
+            Promise.all(jobAddTasks).then(() => {
+                controller.processQueue();
+            });
+            break
+        }
     }
 });
 
